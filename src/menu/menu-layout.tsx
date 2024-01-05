77d5ef1fa547411dc0ddb75f2676de00f7bdaab7
Nikita Snetkov <lukyanish@gmail.com>
2024-01-05 21:36:59 +0600

    Solo game to menu selection was added

--- src/menu/menu-layout.tsx
+++ src/menu/menu-layout.tsx
@@ -35,12 +35,15 @@ export const MenuLayout = () => {
             <div className="left-1/4 absolute h-12 flex items-center w-3/4 px-2 border-b-[2px] border-primary">
                 <span>AskIT.space v.{APP_VERSION}</span>
                 <span className="ml-auto pr-2">Статус сервера:&nbsp;<span className="text-green-500">Онлайн</span></span>
-                <span className="pl-2 border-l-[1px] border-emerald-600">Клиент: {isConnected ? <span className="text-green-500">Подключен</span> : <span className="text-red-500">Отключен</span>}</span>
+                <span className="pl-2 border-l-[2px] border-emerald-600">Клиент: {isConnected ? <span className="text-green-500">Подключен</span> : <span className="text-red-500">Отключен</span>}</span>
             </div>
             {/* Left side */}
             <div className="w-1/4 flex flex-col items-center justify-center border-r-[2px] border-primary rounded-br-[60px] px-12">
                 <RootButton to={PAGES.MENU} className="mb-32">
                     Главная
+                </RootButton>
+                <RootButton to={PAGES.MENU_SOLO_GAME} className="mb-4">
+                    Одиночная игра
                 </RootButton>
                 <RootButton to={PAGES.MENU_JOIN_GAME} className="mb-4">
                     Присоединиться к игре
--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,5 +1,6 @@
 export const PAGES = {
     MENU: "/menu",
+    MENU_SOLO_GAME: "/menu/solo-game",
     MENU_JOIN_GAME: "/menu/join-game",
     MENU_CREATE_GAME: "/menu/create-game",
     LOGIN: "/login",
